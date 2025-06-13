924b6afd1f433708e70a4c5110637cc3c7e9d3c8
Jean-cédric Thérond <jean.cedric.t@gmail.com>
2025-06-13 16:38:22 +0200

    attempt at fixing deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from "@sveltejs/adapter-static";
-import sveltePreprocess from "svelte-preprocess";
+import { sveltePreprocess } from "svelte-preprocess";
 
 const base = process.env.GITHUB_PAGES ? "/escape-games" : "";
 
